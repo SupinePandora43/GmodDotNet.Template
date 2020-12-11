63c2f6477fd88ecccde048b3a00e9742117e4986
SupinePandora43 <supinepandoraggg@gmail.com>
2020-12-11 11:58:50 +0500

    upload template

---
+++ Template/test.lua
@@ -0,0 +1,22 @@
+﻿hook.Add("Tick", "Template_CloseServer", engine.CloseServer)
+
+-- load GmodDotNet
+require("dotnet")
+-- test function
+local function run_test()
+	local module_loaded = dotnet.load("Template")
+	assert(module_loaded==true)
+
+	-- do your test here --
+
+
+
+	-----------------------
+
+	local module_unloaded = dotnet.unload("Template")
+	assert(module_unloaded==true)
+end
+
+run_test()
+
+print("tests are successful!")
